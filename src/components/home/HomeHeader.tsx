510d6fb26875652e09b268a8453d55840d59f71b
NonamedBread <lgh778923@gmail.com>
2024-01-17 22:11:26 +0900

    header 추가

---
+++ src/components/home/HomeHeader.tsx
@@ -0,0 +1,7 @@
+export default function HomeHeader() {
+  return (
+    <div className="home-header bg-blue-500 text-white p-5 text-center">
+      <h1 className="text-4xl">Home</h1>
+    </div>
+  );
+}
