47badcf098e021966ecb6c4d60445bfc1c306c10
NonamedBread <lgh778923@gmail.com>
2024-01-28 14:33:41 +0900

    Refactor ToggleSwitch component and add dark mode support

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,45 +1,41 @@
-import React, { useState } from "react";
-import NightsStayIcon from "@mui/icons-material/NightsStay";
-import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import React from "react";
+
 interface ToggleSwitchProps {
-  theme: string;
-  toggleTheme: () => void;
+  toggleAction: () => void;
+  icons: {
+    [key: string]: React.ElementType;
+  };
+  checked: boolean;
 }
 
-function ToggleSwitch({ theme, toggleTheme }: ToggleSwitchProps) {
+export default function ToggleSwitch({
+  icons,
+  checked,
+  toggleAction,
+}: ToggleSwitchProps) {
   const handleToggle = () => {
-    toggleTheme();
+    toggleAction();
   };
 
+  const Icon = checked ? icons.checked : icons.unChecked;
+
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center">
       <label
         htmlFor="toggle"
-        className={`relative w-24 h-12 rounded-full cursor-pointer ${
-          theme === "dark" ? "bg-gray-800" : "bg-yellow-500"
-        }`}
+        className="relative w-24 h-12 rounded-full cursor-pointer bg-yellow-500 dark:bg-gray-800"
       >
         <input
           type="checkbox"
           id="toggle"
-          className="sr-only"
-          checked={theme === "light" ? false : true}
+          className="sr-only  cursor-none"
+          checked={checked}
           onChange={handleToggle}
         />
-        <span
-          className={`absolute left-0 inline-block w-12 h-12  rounded-full shadow-lg transform transition-transform duration-300 flex items-center justify-center ${
-            theme === "dark" ? "translate-x-full bg-gray-900" : "bg-slate-50"
-          }`}
-        >
-          {theme === "dark" ? (
-            <NightsStayIcon className="h-8 w-8 text-slate-100" />
-          ) : (
-            <WbSunnyIcon className="h-8 w-8 text-yellow-500" />
-          )}
+        <span className="absolute left-0 inline-block w-12 h-12 rounded-full shadow-lg transform transition-transform duration-300 flex items-center justify-center bg-slate-50 dark:translate-x-full dark:bg-gray-900">
+          <Icon className="h-8 w-8 text-yellow-500 dark:text-slate-100" />
         </span>
       </label>
     </div>
   );
 }
-
-export default ToggleSwitch;
