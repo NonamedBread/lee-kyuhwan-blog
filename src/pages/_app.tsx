0e2172d701f3e8425d0f4b554ffa710e9cd6dd87
NonamedBread <lgh778923@gmail.com>
2024-01-29 09:40:56 +0900

    Add dark mode support to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
@@ -5,14 +7,33 @@ import { Provider } from "react-redux";
 import { wrapper } from "@/modules/store";
 import Layout from "@/components/home/Layout";
 
+interface Props {
+  children: React.ReactNode;
+}
+
+function ThemeWrapper({ children }: Props) {
+  const theme = useSelector((state: any) => state.darkMode.theme);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return children;
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
-
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeWrapper>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeWrapper>
     </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,4 @@
-import { useEffect, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
-
 export default function Home() {
-  const theme = useSelector((state: any) => state.darkMode.theme);
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-red-100">
       <h1 className="text-6xl font-bold">Posts</h1>
