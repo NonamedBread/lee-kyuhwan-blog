2a65c4b871a743d0850cf53cdfee93c72b94b1ed
NonamedBread <lgh778923@gmail.com>
2024-01-29 12:57:32 +0900

    Refactor Home component and add getStaticProps
    This commit refactors the Home component in the index.tsx file and adds a new getStaticProps function to fetch dummy data from an API.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { GetStaticProps } from "next";
+
+interface Props {
+  posts: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }[];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-red-100">
       <h1 className="text-6xl font-bold">Posts</h1>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  // dummy data
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
