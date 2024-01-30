5e4fc631ceff6419e199e949d4a90b223840ba61
NonamedBread <lgh778923@gmail.com>
2024-01-30 12:57:11 +0900

    Refactor height styles and adjust post card width

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -6,15 +6,11 @@ interface Post {
 }
 
 export default function Posts({ posts }: { posts: Post[] }) {
-  console.log("posts", posts);
   return (
     <div className="flex flex-wrap justify-center min-h-screen py-2 ">
       {posts.map((post) => (
-        <div key={post.id} className="w-1/4 p-4">
-          <div
-            style={{ height: "35vh" }}
-            className="p-6 rounded shadow overflow-auto bg-white dark:bg-gray-900"
-          >
+        <div key={post.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+          <div className="h-full p-6 rounded shadow overflow-auto bg-white dark:bg-gray-900">
             <h2 className="font-bold mb-2">{post.title}</h2>
             <p>{post.body}</p>
           </div>
