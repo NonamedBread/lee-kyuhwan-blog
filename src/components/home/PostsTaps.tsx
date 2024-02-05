72cbe860c0b707a9935350701e300679f8b4fe7c
NonamedBread <lgh778923@gmail.com>
2024-02-05 09:49:09 +0900

    Refactor clip path in PostsTaps component

--- src/components/home/PostsTaps.tsx
+++ src/components/home/PostsTaps.tsx
@@ -1,33 +1,26 @@
 export default function PostsTaps() {
+  //TODO : clip path tailwindcss로 이동
   return (
-    <div className="flex justify-start items-center  space-x-4 px-5 bg-gray-200 dark:bg-customGreay-800">
-      <div
-        className="text-gray-500 bg-gray-300 w-20 p-0.5 relative group hover:text-gray-800 hover:bg-gray-500 hover:z-10"
-        style={{
-          clipPath: "polygon(17% 5%, 83% 5%, 97% 100%, 3% 100%)",
-        }}
-      >
+    <div className="flex justify-start items-center  space-x-4 px-5 ">
+      <div className="text-gray-500 bg-gray-300 w-20 p-0.5 relative group hover:text-gray-800 hover:bg-gray-500 hover:z-10 [clip-path:polygon(17%_5%,_83%_5%,_97%_100%,_3%_100%)]">
         <div
-          className="w-full h-full bg-white flex items-center justify-center"
+          className="w-full h-full bg-white flex items-center justify-center [clip-path:polygon(17%_5%,_83%_5%,_97%_100%,_3%_100%)]"
           style={{
-            clipPath: "polygon(17% 5%, 83% 5%, 97% 100%, 3% 100%)",
             zIndex: "-1",
           }}
         >
           All
         </div>
       </div>
-      <div className="text-gray-500 bg-gray-300 px-2 py-1 relative group hover:text-gray-800 hover:bg-gray-500 hover:z-10">
-        Tags
-        {/* <div
-          className="absolute top-0 left-0 w-full h-full bg-white"
+      <div className="text-gray-500 bg-gray-300 w-20 p-0.5 relative group hover:text-gray-800 hover:bg-gray-500 hover:z-10 [clip-path:polygon(17%_5%,_83%_5%,_97%_100%,_3%_100%)]">
+        <div
+          className="w-full h-full bg-white flex items-center justify-center [clip-path:polygon(17%_5%,_83%_5%,_97%_100%,_3%_100%)]"
           style={{
-            clipPath: "polygon(17% 5%, 83% 5%, 97% 95%, 3% 95%)",
             zIndex: "-1",
           }}
         >
-          tags2
-        </div> */}
+          Tags
+        </div>
       </div>
     </div>
   );
