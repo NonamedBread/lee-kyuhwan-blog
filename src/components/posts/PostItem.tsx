e76d74e3d3dac2857ead7c12238415d58c558f52
NonamedBread <lgh778923@gmail.com>
2024-02-08 10:20:24 +0900

    포스트 아이템과 포스트 디테일 페이지에 관련된 코드 변경 사항 추가

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Post {
   userId: number;
@@ -16,31 +17,33 @@ export default function PostItem({ post }: { post: Post }) {
   let formattedDate = yyyy + '년 ' + mm + '월 ' + dd + '일';
   return (
     <div key={post.id} className="h-[45dvh] w-full cursor-pointer p-4 md:w-1/2 lg:w-1/3 xl:w-1/4">
-      <div className="h-full overflow-auto rounded bg-white shadow dark:bg-customGreay-800">
-        <div className="relative h-2/5 rounded">
-          <Image className=" object-cover" src="/images/dummy_image.png" fill priority alt={post.title} sizes="100%" />
-        </div>
-        <div className="h-3/5 w-full p-4 ">
-          <div>
-            <h2 className="mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap font-bold">{post.title}</h2>
+      <Link href={`/posts/${post.id}`}>
+        <div className="h-full overflow-auto rounded bg-white shadow dark:bg-customGreay-800">
+          <div className="relative h-2/5 rounded">
+            <Image className=" object-cover" src="/images/dummy_image.png" fill priority alt={post.title} sizes="100%" />
           </div>
-          <p
-            className="h-28 overflow-hidden"
-            style={{
-              display: '-webkit-box',
-              WebkitLineClamp: 5,
-              WebkitBoxOrient: 'vertical',
-            }}
-          >
-            {post.body}
-          </p>
-          <div className="my-4 w-full border-t border-gray-200"></div>
-          <div className="mt-4 flex  justify-between">
-            <p className="text-sm text-gray-500">{formattedDate}</p>
-            <p className="ml-4 text-sm text-gray-500">{post.id}개의 댓글</p>
+          <div className="h-3/5 w-full p-4 ">
+            <div>
+              <h2 className="mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap font-bold">{post.title}</h2>
+            </div>
+            <p
+              className="h-28 overflow-hidden"
+              style={{
+                display: '-webkit-box',
+                WebkitLineClamp: 5,
+                WebkitBoxOrient: 'vertical',
+              }}
+            >
+              {post.body}
+            </p>
+            <div className="my-4 w-full border-t border-gray-200"></div>
+            <div className="mt-4 flex  justify-between">
+              <p className="text-sm text-gray-500">{formattedDate}</p>
+              <p className="ml-4 text-sm text-gray-500">{post.id}개의 댓글</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 export default function PostDetailPage() {
   const router = useRouter();
   const { slug } = router.query;
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (slug) {
+      const fetchPost = async () => {
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+          .then((res) => res.json())
+          .then((data) => setPost(data));
+      };
+
+      fetchPost();
+    }
+  }, [slug]);
+
+  console.log(post);
 
   return <div className="flex min-h-screen flex-col items-center justify-center">{slug}</div>;
 }
