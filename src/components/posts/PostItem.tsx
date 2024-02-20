d5f21621786f8acc319f70e70c56a1b613f6ab96
NonamedBread <lgh778923@gmail.com>
2024-02-20 13:05:27 +0900

    PostItem 컴포넌트에서 content 길이 조절 및 Markdown 렌더링 수정

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -31,24 +31,19 @@ export default function PostItem({ post }: { post: Post }) {
           <div>
             <h2 className="mb-2 overflow-hidden overflow-ellipsis whitespace-nowrap font-bold">{post.title}</h2>
           </div>
-
-          {/* content 길이 조절 */}
-          {/* <p
+          <div
             className="h-28 overflow-hidden"
             style={{
               display: '-webkit-box',
               WebkitLineClamp: 5,
               WebkitBoxOrient: 'vertical',
             }}
-          > */}
-          {/* {post.content} */}
-          <MarkdownRenderer>{post.content}</MarkdownRenderer>
-          {/* {typeof window !== 'undefined' && <ReactMarkdown>{post.content}</ReactMarkdown>}{' '} */}
-          {/* </p> */}
+          >
+            <MarkdownRenderer>{post.content}</MarkdownRenderer>
+          </div>
           <div className="my-4 w-full border-t border-gray-200"></div>
           <div className="mt-4 flex  justify-between">
             <p className="text-sm text-gray-500">{formattedDate}</p>
-            {/* <p className="ml-4 text-sm text-gray-500">{post.id}개의 댓글</p> */}
           </div>
         </div>
       </div>
