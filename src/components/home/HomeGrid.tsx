df86eac7a884c65b750b9991753bb8bea0a59c37
NonamedBread <lgh778923@gmail.com>
2024-03-10 13:35:38 +0900

    FEAT 검색결과 없을 경우 보여줄 화면 추가

--- src/components/home/HomeGrid.tsx
+++ src/components/home/HomeGrid.tsx
@@ -31,31 +31,31 @@ export default function HomeGrid({ posts, allTags }: Props) {
   const handleTagClick = useCallback(
     (tag: string) => {
       setSelectedTag(tag);
-      console.log('tag:', tag);
       dispatch(setSearchedTags(tag));
     },
     [dispatch],
   );
 
-  // selectedTag이 변경될 때마다 검색된 태그를 변경
-  // useEffect(() => {
-  //   console.log('selectedTag:', selectedTag);
-  //   if (!selectedTag) {
-  //     dispatch(setSearchedTags(''));
-  //     return;
-  //   }
-  //   dispatch(setSearchedTags(selectedTag));
-  // }, [selectedTag]);
-
   return (
-    <div className="flex flex-col items-center justify-center space-y-8">
+    <div className="flex w-full flex-col items-center justify-center space-y-8">
       <HomeSearch allTags={allTags} selectedTag={selectedTag} handleTagClick={handleTagClick} />
       <div className="w-[60%]">
         <Taps />
         <div className="flex min-h-screen flex-wrap space-y-2 rounded-md border border-customGreay-200 p-2 dark:border-customGreay-100">
-          {posts.map((post) => (
-            <PostItem key={post.slug} post={post} handleTagClick={handleTagClick} />
-          ))}
+          {posts && posts.length > 0 ? (
+            posts.map((post) => <PostItem key={post.slug} post={post} handleTagClick={handleTagClick} />)
+          ) : (
+            <div className="flex h-full w-full items-center justify-center bg-gray-100 text-2xl font-bold text-gray-500">
+              <div className="m-20 flex flex-col items-center">
+                <span className="text-2xl font-bold">
+                  아쉽게도 검색 결과가 없네요.
+                  <span role="img" aria-label="Sad face" className="text-4xl">
+                    🫨
+                  </span>
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
