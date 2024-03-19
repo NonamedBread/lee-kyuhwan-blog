d55de9a968b75644b30e47d973caa001cd769f44
NonamedBread <lgh778923@gmail.com>
2024-03-19 13:01:21 +0900

    FEAT 홈 헤더 컴포넌트 sideTap toggle switch 추가,   Tailwind CSS 설정 변경

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 
 import NightsStayIcon from '@mui/icons-material/NightsStay';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import LabelIcon from '@mui/icons-material/Label';
+import LabelOffIcon from '@mui/icons-material/LabelOff';
+import PersonIcon from '@mui/icons-material/Person';
 
 import ToggleSwitch from '../ToggleSwitch';
 
@@ -12,9 +14,14 @@ interface Props {
   theme: string;
 }
 
-const icons = {
+const DarkModeicons = {
   checked: NightsStayIcon,
   unChecked: WbSunnyIcon,
+};
+
+const SideTapIcons = {
+  checked: LabelIcon,
+  unChecked: LabelOffIcon,
 };
 
 //TODO : 사이드탭 추가, 툴팁 추가
@@ -27,11 +34,10 @@ const HomeHeader = forwardRef<HTMLDivElement, Props>(({ toggleTheme, theme }, re
       <Link href={'/'}>
         <h1 className="text-4xl font-bold">{'< Lee`s Devlog >'}</h1>
       </Link>
-      <div className="flex items-center gap-9 pr-6">
-        <ToggleSwitch toggleAction={toggleTheme} icons={icons} checked={checked} />
-        <Link href={'https://github.com/NonamedBread'} passHref>
-          <GitHubIcon className="cursor-pointer text-5xl" />
-        </Link>
+      <div className="flex items-center gap-9">
+        <ToggleSwitch toggleAction={toggleTheme} icons={SideTapIcons} checked={checked} />
+        <ToggleSwitch toggleAction={toggleTheme} icons={DarkModeicons} checked={checked} />
+        <PersonIcon className="text-customBlue-500 dark:text-customBlue-100 cursor-pointer text-5xl" />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         customGreay: {
-          100: "#f5f5f5",
-          200: "#e5e5e5",
-          300: "#d4d4d4",
-          400: "#a3a3a3",
-          500: "#737373",
-          600: "#525252",
-          700: "#404040",
-          800: "#262626",
-          900: "#171717",
+          100: '#f5f5f5',
+          200: '#e5e5e5',
+          300: '#d4d4d4',
+          400: '#a3a3a3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+        },
+        customBlue: {
+          100: '#e6f7ff',
+          200: '#bae7ff',
+          300: '#91d5ff',
+          400: '#69c0ff',
+          500: '#40a9ff',
+          600: '#1890ff',
+          700: '#0d80ff',
+          800: '#0050b3',
+          900: '#003366',
         },
       },
       clipPath: {
-        tap: "polygon(17% 5%, 83% 5%, 97% 100%, 3% 100%)",
+        tap: 'polygon(17% 5%, 83% 5%, 97% 100%, 3% 100%)',
       },
     },
   },
