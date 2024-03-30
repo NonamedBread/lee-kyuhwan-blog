b6148f929b1146081357046bd46c06f25412d114
NonamedBread <lgh778923@gmail.com>
2024-03-30 13:27:55 +0900

    FEAT closeAll 아이콘 추가

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -7,24 +7,25 @@ interface DropdownProps {
     seriesName: string;
     posts: { constent: string; date: string; isDraft: boolean; slug: string; tags: string[]; title: string }[];
   };
-  onClickEvent: () => void;
+  onTitleClickEvent: () => void | undefined;
 }
-export default function Dropdown({ visibility, items, onClickEvent }: DropdownProps) {
+export default function Dropdown({ visibility, items, onTitleClickEvent }: DropdownProps) {
   return (
-    <div className="flex w-full flex-col gap-2 " key={items.seriesName}>
-      <div className="flex cursor-pointer items-center gap-1" onClick={onClickEvent}>
+    <div className="flex w-full flex-col" key={items.seriesName}>
+      <div className="flex cursor-pointer items-center  p-1" onClick={onTitleClickEvent}>
         <ExpandMoreIcon className={`text-2xl ${visibility ? '' : '-rotate-90'}`} />
         <h2 className="text-xl font-bold">{items.seriesName}</h2>
       </div>
-      <article className={'transform overflow-hidden'}>
+      <article className={'transform overflow-hidden p-1'}>
         <ul className={` ${visibility ? 'slide-in-sideTap' : 'slide-out-sideTap'} `}>
           {items.posts.map((item: DropdownProps['items']['posts'][0]) => (
-            <li key={item.title} className="flex cursor-pointer items-center justify-between p-2 hover:bg-gray-100 dark:hover:bg-customGreay-800">
-              <span>{item.title}</span>
+            <li key={item.title} className="flex cursor-pointer items-center justify-between p-2 hover:bg-gray-200 dark:hover:bg-customGreay-600">
+              <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">{item.title}</span>
             </li>
           ))}
         </ul>
       </article>
+      <div className="w-full border-b-2  border-gray-300"></div>
     </div>
   );
 }
--- src/components/home/HomeSideTap.tsx
+++ src/components/home/HomeSideTap.tsx
@@ -2,9 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { Tooltip } from '@mui/material';
+import { Tooltip, IconButton } from '@mui/material';
 import TagIcon from '@mui/icons-material/Tag';
-
+import IndeterminateCheckBoxOutlinedIcon from '@mui/icons-material/IndeterminateCheckBoxOutlined';
 import Dropdown from '../common/Dropdown';
 
 interface Props {
@@ -26,6 +26,15 @@ export default function HomeSideTap({ isHeaderVisible, sideTap }: Props) {
     setDropdownOpen(newDropdownOpen);
   }, [series]);
 
+  const closeAllDropdowns = () => {
+    const newDropdownOpen = Object.keys(dropdownOpen).reduce((acc: Record<string, boolean>, cur: string) => {
+      acc[cur] = false;
+      return acc;
+    }, {});
+
+    setDropdownOpen(newDropdownOpen);
+  };
+
   const toggleDropdown = (seriesName: string) => {
     setDropdownOpen((prevState) => ({ ...prevState, [seriesName]: !prevState[seriesName] }));
   };
@@ -43,23 +52,25 @@ export default function HomeSideTap({ isHeaderVisible, sideTap }: Props) {
       className={`${animationClass} fixed left-0 top-0 z-50  ${isHeaderVisible ? '' : 'mt-[10dvh]'} h-screen w-[18dvw]  transition-all duration-300 ease-in-out `}
     >
       <div
-        className={` flex h-full flex-col items-center gap-4 ${roundedClasses} ${borderClasses} border-gray-300 bg-slate-100 p-6 dark:bg-customGreay-900  `}
+        className={` flex h-full flex-col items-center gap-3 ${roundedClasses} ${borderClasses} border-gray-300 bg-slate-100 p-6 dark:bg-customGreay-900  `}
       >
         <div className="flex w-full flex-col gap-1 ">
-          <div className="flex items-center">
+          <div className="flex items-center p-1">
             <TagIcon className="text-2xl" />
             <h1 className="text-2xl font-bold">시리즈</h1>
+            <IconButton onClick={closeAllDropdowns} className="ml-auto cursor-pointer" size="small">
+              <IndeterminateCheckBoxOutlinedIcon />
+            </IconButton>
           </div>
           <div className="w-full border-b-2  border-gray-300"></div>
         </div>
-        {series.map((seriesItem: any, index: number) => (
+        {series.map((seriesItem: any) => (
           <>
             <Dropdown
               visibility={dropdownOpen[seriesItem.seriesName]}
               items={seriesItem}
-              onClickEvent={() => toggleDropdown(seriesItem.seriesName)}
+              onTitleClickEvent={() => toggleDropdown(seriesItem.seriesName)}
             />
-            <div className="w-full border-b-2  border-gray-300"></div>
           </>
         ))}
       </div>
