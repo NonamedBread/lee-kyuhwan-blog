74a2bdebc3e6250568b1d80a6d1b9dace057632a
NonamedBread <lgh778923@gmail.com>
2024-05-05 13:21:36 +0900

    FEAT: Main page date position, tag appearance

--- src/components/home/HomeTag.tsx
+++ src/components/home/HomeTag.tsx
@@ -1,3 +1,5 @@
+import { Tooltip } from '@mui/material';
+
 interface HomeTagsProps {
   tags: {
     name: string;
@@ -10,47 +12,66 @@ interface HomeTagsProps {
   textColor?: 'white' | 'black' | 'gray';
 }
 
-export default function HomeTag({ tags, handleTagClick, handleSideTap, size = 'md', bgColor = 'blue', textColor = 'white' }: HomeTagsProps) {
-  const sizeClasses = {
-    sm: 'px-2 py-1',
-    md: 'px-4 py-2',
-    lg: 'px-6 py-3',
-  };
+const SIZE_CLASSES = {
+  sm: 'px-3 py-2',
+  md: 'px-4 py-2',
+  lg: 'px-6 py-3',
+};
 
-  const bgColorClasses = {
-    blue: 'bg-customBlue-500 hover:bg-customBlue-600',
-    red: 'bg-customRed-500 hover:bg-customRed-600',
-    green: 'bg-customGreen-500 hover:bg-customGreen-600',
-    slate: 'bg-slate-200 hover:bg-slate-300 ',
-  };
+const BG_COLOR_CLASSES = {
+  blue: 'bg-customBlue-500 hover:bg-customBlue-600',
+  red: 'bg-customRed-500 hover:bg-customRed-600',
+  green: 'bg-customGreen-500 hover:bg-customGreen-600',
+  slate: 'bg-slate-200 hover:bg-slate-300 ',
+};
 
-  const textColorClasses = {
-    white: 'text-white',
-    black: 'text-black dark:text-slate-100 dark:hover:text-slate-200',
-    gray: 'text-gray-500 ',
-  };
+const TEXT_COLOR_CLASSES = {
+  white: 'text-white',
+  black: 'text-black dark:text-slate-100 dark:hover:text-slate-200',
+  gray: 'text-gray-500 ',
+};
 
-  const textSizeClasses = {
-    sm: 'text-sm',
-    md: 'text-md',
-    lg: 'text-lg',
-  };
+const TEXT_SIZE_CLASSES = {
+  sm: 'text-[13px]',
+  md: 'text-[15px]',
+  lg: 'text-lg',
+};
 
+export default function HomeTag({
+  tags,
+  handleTagClick,
+  handleSideTap = () => {},
+  size = 'md',
+  bgColor = 'blue',
+  textColor = 'white',
+}: HomeTagsProps) {
   return (
-    <div className="flex flex-wrap justify-center gap-2">
+    <div className="flex flex-wrap justify-center gap-3">
       {tags.map((tag) => (
         <button
           key={tag.name}
           onClick={() => {
             handleTagClick(tag.name);
-            handleSideTap && handleSideTap(true);
+            handleSideTap(true);
           }}
-          className={`m-1 rounded-full transition-all duration-300 ease-in-out hover:cursor-pointer
-          dark:bg-customGreay-600 dark:hover:bg-customGreay-700
-          ${sizeClasses[size]} ${bgColorClasses[bgColor]} ${textColorClasses[textColor]} ${textSizeClasses[size]}
-          `}
+          className={[
+            'w-max',
+            'rounded-full',
+            'transition-all',
+            'duration-300',
+            'ease-in-out',
+            'hover:cursor-pointer',
+            'dark:bg-customGreay-600',
+            'dark:hover:bg-customGreay-700',
+            SIZE_CLASSES[size],
+            BG_COLOR_CLASSES[bgColor],
+            TEXT_COLOR_CLASSES[textColor],
+            TEXT_SIZE_CLASSES[size],
+          ].join(' ')}
         >
-          {tag.name}
+          <Tooltip title={tag.name} arrow placement="top">
+            <span>{tag.name.length > 12 ? `${tag.name.substring(0, 12)}...` : tag.name}</span>
+          </Tooltip>
         </button>
       ))}
     </div>
