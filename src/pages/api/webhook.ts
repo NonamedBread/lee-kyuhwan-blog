9e8fd72da2b11a9639e749d80a7c42ad056d6e97
NonamedBread <lgh778923@gmail.com>
2024-06-07 08:59:10 +0900

    chore: Add logging statements for debugging in webhook handler

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -3,13 +3,17 @@ import { getPostsGroupedBySeries } from '@/lib/postUtils';
 import crypto from 'crypto';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Webhook invoked'); // 웹훅 호출 로그
+
   if (req.method === 'POST') {
     const signature = req.headers['x-hub-signature'];
     const payload = JSON.stringify(req.body);
 
     const secret = process.env.WEBHOOK_SECRET;
+    console.log('secret:', secret);
 
     if (!secret) {
+      console.log('WEBHOOK_SECRET is not defined'); // 시크릿 미정의 로그
       return res.status(500).json({ message: 'WEBHOOK_SECRET is not defined' });
     }
 
@@ -18,6 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // 시그니처 검증
     if (signature !== digest) {
+      console.log('Invalid signature'); // 시그니처 검증 실패 로그
       return res.status(403).json({ message: 'Invalid signature' });
     }
 
@@ -25,9 +30,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // 게시물 데이터를 가져오는 함수 호출
       const allSeries = await getPostsGroupedBySeries();
 
+      console.log('Revalidating');
+
       return res.status(200).json({ message: 'Success', series: allSeries });
     } catch (error) {
-      console.log('Error revalidating');
+      console.log('Error revalidating:', error); // 데이터 가져오기 오류 로그
       if (error instanceof Error) {
         return res.status(500).json({ message: 'Error revalidating', error: error.message });
       } else {
@@ -35,6 +42,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     }
   } else {
+    console.log('Method Not Allowed'); // 허용되지 않은 메서드 로그
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
